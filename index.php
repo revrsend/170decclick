9c9b584b83085fb0296d552c5f2c64ba04ed8da8
revrsend <iresponcetama@gmail.com>
2024-10-08 00:08:04 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0; url=http://kolsatra.site/4IFXpy2316RWtWt110eTgP0wqJ0vPpu0SkEO9">
+    <title>170 Piece Survival Kit</title>
+</head>
+<body>
+    <!-- no content in the body -->
+</body>
+</html>
